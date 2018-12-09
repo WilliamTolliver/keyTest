72584b43ebebc3115c7d36db531c8d001465b1c3
CMH\wtolliver <WTolliver@netjets.com>
2018-12-09 04:24:17 -0500

    update keypress again

--- keyInputTester.py
+++ keyInputTester.py
@@ -4,18 +4,13 @@ Created on Sat Dec  8 23:08:35 2018
 
 @author: wtolliver
 """
-
-import pygame, sys
-pygame.init()
-screen = pygame.display.set_mode([640,480])
-clock = pygame.time.Clock()
-
-while 1:
-    clock.tick(60) #60 FPS
-    for event in pygame.event.get:
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                print "The Spacebar was pressed."
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
-                print "The Spacebar was released."
+import keyboard #Using module keyboard
+while True:#making a loop
+    try: #used try so that if user pressed other than the given key error will not be shown
+        if keyboard.is_pressed('a'): #if key 'a' is pressed 
+            print('You Pressed A Key!')
+            break #finishing the loop
+        else:
+            pass
+    except:
+        break #if user pressed other than the given key the loop will break
