617c355d8153119a49bba8a1ab9185dd0c66b2a8
CMH\wtolliver <WTolliver@netjets.com>
2018-12-27 01:41:29 -0500

    fixed file location

--- keyInputTester.py
+++ keyInputTester.py
@@ -5,33 +5,39 @@ Created on Sat Dec  8 23:08:35 2018
 @author: wtolliver
 """
 
-from threading import Thread
+from pad4pi import rpi_gpio
+import time
 
-""" Declarations
+#Setup Keypad right now
+KEYPAD = [
+          ["1","2","3","A"],
+          ["4","%","6","B"],
+          ["7","8","9","C"],
+          ["*","0","#","D"]
+]
+          
+#Same as calling: factory.create_4_by_4_keypad, still we put here fyi
+ROW_PINS = [4, 14, 15, 17] # BCM numbering
+COL_PINS = [18, 27, 22, 23] # BCM Numbering
 
-"""
-threads = []
+factory = rpi_gpio.KeypadFactory()
 
-def update():
+keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PINS)
+
+#keypad.cleanup
+
+def printKey(key):
+    print(key)
+    if(key=="1"):
+        print("number")
+    elif (key=="A"):
+        print("letter")
+        
+# printKey will be callec each time a keypad button is pressed
+        
+try:
+    while(True):
+        time.sleep(0.2)
+except:
+    keypad.cleanup()
     
-    frameCount = 0
-    fps = 1.0/16.67
-    code = 1234
-    code2 = 5678
-    
-    while (frameCount <= 60):
-        frameCount = frameCount + 1
-        time.sleep(fps)
-        
-        print(frameCount)
-        
-        if(frameCount == 60):
-            code = code2
-            print(code)
-        
-        if(code == 5678):
-            print("You've successfully cracked the code!!!")
-            break
-
-process = Thread(target=update, args=[])
-process.start()
