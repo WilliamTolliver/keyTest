8616d20b38911e273154d1146dcc0e2066c12fc6
CMH\wtolliver <WTolliver@netjets.com>
2018-12-27 01:11:56 -0500

    Using New raspPi pad4Pi stuff.

--- keyInputTester.py
+++ keyInputTester.py
@@ -4,13 +4,34 @@ Created on Sat Dec  8 23:08:35 2018
 
 @author: wtolliver
 """
-import keyboard #Using module keyboard
-while True:#making a loop
-    try: #used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('a'): #if key 'a' is pressed 
-            print('You Pressed A Key!')
-            break #finishing the loop
-        else:
-            pass
-    except:
-        break #if user pressed other than the given key the loop will break
+
+from threading import Thread
+
+""" Declarations
+
+"""
+threads = []
+
+def update():
+    
+    frameCount = 0
+    fps = 1.0/16.67
+    code = 1234
+    code2 = 5678
+    
+    while (frameCount <= 60):
+        frameCount = frameCount + 1
+        time.sleep(fps)
+        
+        print(frameCount)
+        
+        if(frameCount == 60):
+            code = code2
+            print(code)
+        
+        if(code == 5678):
+            print("You've successfully cracked the code!!!")
+            break
+
+process = Thread(target=update, args=[])
+process.start()
